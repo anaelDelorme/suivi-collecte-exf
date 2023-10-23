b9f3ac54629af474f6672c64278713ff3c161d2d
Anaël Delorme <87323681+anaelDelorme@users.noreply.github.com>
2023-10-23 17:17:16 +0200

    Update app_ui.R

--- suiviCollecte/R/app_ui.R
+++ suiviCollecte/R/app_ui.R
@@ -60,7 +60,7 @@ golem_add_external_resources <- function() {
     favicon(),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = "testGolemAuth"
+      app_title = "Suivi de collecte EXF"
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
